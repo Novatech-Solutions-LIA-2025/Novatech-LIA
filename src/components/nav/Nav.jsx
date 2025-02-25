95a95cf4d2b3b49cc2441ef6f746c4eddff209f1
nihlen <johan.nihlen@utb.ecutbildning.se>
2025-02-25 22:05:48 +0100

    Refactor project structure: update jsconfig, add layout, header, footer, and navigation components, and apply global styles

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,20 @@
+import "./nav.css";
+
+function Nav() {
+  return (
+    <div >
+      <nav className="navbar">
+      <h1>Logo</h1>
+        <ul className="nav-list">
+          <li>Kollegor</li>
+          <li>Tjänster</li>
+          <li>Insikt</li>
+          <li>Kontakt</li>
+          <li>Blogg</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Nav
