dae17006c01f70c1dfd4880c703366c607faf502
nihlen <johan.nihlen@utb.ecutbildning.se>
2025-02-28 15:11:25 +0100

    Refactor header and navigation components; replace logo text with image, update styles, and remove unused SVG files.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,10 +2,15 @@ import "./header.css";
 
 function Header() {
   return (
-    <div className="hero">
+    <div className="hero-border-shadow">
       <header>
-        <h1 className="hero-text">Växla Upp Med <span className="hero-highlight">Ny Tech</span> <br></br>
-        - Framtiden Är Nu</h1>
+        <h1 className="hero-text">
+          Växla Upp Med <span className="hero-highlight">Ny Tech</span>{" "}
+          <br></br>- Framtiden Är Nu
+        </h1>
+        <div className="hero">
+          <p></p>
+        </div>
       </header>
     </div>
   );
