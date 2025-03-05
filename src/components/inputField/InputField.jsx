b5b06a4b103c91e5b0cebc6d872e9af2c2f46465
michel <michel.gorjizadeh@gmail.com>
2025-03-05 18:31:38 +0100

    Add InputField component and update footer styles for improved layout

---
+++ src/components/inputField/InputField.jsx
@@ -0,0 +1,26 @@
+import "./inputField.css";
+
+
+function InputField() {
+  return (
+
+    <div className="inputField">
+      <h6>
+        <li className="listHighlite"></li>
+        Kontakta oss
+      </h6>
+
+      <h1>
+        <span className="headlineHighlite">Gratis</span> konsulation
+        <div className="underscore"></div>
+      </h1>
+    </div>
+    
+    
+    
+
+
+  );
+}
+
+export default InputField;
