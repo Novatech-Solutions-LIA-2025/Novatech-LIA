a58856c857ddd111d4089a4c9ab08153addfeb47
nihlen <johan.nihlen@utb.ecutbildning.se>
2025-03-07 09:51:37 +0100

    Add MainComponent and SmallCard with styling; update header shadow effects

---
+++ src/components/main/MainComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SmallCard from "../cards/smallCard/SmallCard";
+
+function MainComponent() {
+  return (
+    <>
+      <div>
+      <SmallCard  title="Rådgivning och Workshops" text="Från tanke till verklighet - strategisk planering i
+samarbete med kund 1" backgroundClass="smallbg1" />
+      </div>
+    </>
+  );
+}
+
+export default MainComponent;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from "../components/header/Header";
+import MainComponent from '../components/main/MainComponent';
 
 function Landing() {
   return (
     <div>
       <Header></Header>
+      <MainComponent></MainComponent>
     </div>
   )
 }
