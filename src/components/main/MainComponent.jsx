f645144a73af2952a58ea38b7564d7d694d7567a
nihlen <johan.nihlen@utb.ecutbildning.se>
2025-03-12 09:31:24 +0100

    Add InputField and Insite components to MainComponent

--- src/components/main/MainComponent.jsx
+++ src/components/main/MainComponent.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import SmallCard from "../cards/smallCard/SmallCard";
 import "./mainComponent.css";
 import Colleagues from "../colleagues/Colleagues";
+import InputField from "../inputField/InputField";
+import Insite from "../insite/Insite";
 
 
 function MainComponent() {
@@ -11,6 +13,8 @@ function MainComponent() {
       <SmallCard  title="Rådgivning och Workshops" text="Från tanke till verklighet - strategisk planering i
 samarbete med kund" smBackgroundClass="smallbg1" smIconClass="smallicon1"/>
       <Colleagues></Colleagues>
+      <Insite></Insite>
+      <InputField></InputField>
       </div>
     </>
   );
