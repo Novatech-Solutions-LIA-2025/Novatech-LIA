b2ab62504a2f95c130ecdb61b8675b270b953c70
michel <michel.gorjizadeh@gmail.com>
2025-03-17 11:09:01 +0100

    Update CSS max-width for input field, insite, and services components; improve layout responsiveness and consistency

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 import Header from "../components/header/Header";
-import MainComponent from '../components/main/MainComponent';
-import Colleagues from '../components/colleagues/Colleagues';
-import Services from '../components/ourServices/Services';
-import InputField from '../components/inputField/InputField';
-import Insite from '../components/insite/Insite';
+import MainComponent from "../components/main/MainComponent";
+import Colleagues from "../components/colleagues/Colleagues";
+import Services from "../components/ourServices/Services";
+import InputField from "../components/inputField/InputField";
+import Insite from "../components/insite/Insite";
 
 function Landing() {
   return (
@@ -13,10 +13,10 @@ function Landing() {
       {/* <MainComponent></MainComponent> */}
       <Colleagues></Colleagues>
       <Services></Services>
-      <InputField></InputField>
       <Insite></Insite>
+      <InputField></InputField>
     </div>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
