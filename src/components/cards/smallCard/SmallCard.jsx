43bf44a7bfbd58e2dead4857f53376d4b56f8f06
nihlen <johan.nihlen@utb.ecutbildning.se>
2025-03-17 21:08:36 +0100

    Refactor SmallCard component structure and styles; enhance layout in Header and update CSS for colleagues and services components

--- src/components/cards/smallCard/SmallCard.jsx
+++ src/components/cards/smallCard/SmallCard.jsx
@@ -1,25 +1,21 @@
-import React from 'react'
-import './smallCard.css'
+import React from "react";
+import "./smallCard.css";
 
-function SmallCard({title, text, smBackgroundClass, smIconClass}) {
+function SmallCard({ title, text, smBackgroundClass, smIconClass }) {
   return (
     <>
-    <div className="smcard-border">
-      <div className="blackbackground">
-    <div className={`small-card-container ${smBackgroundClass}`}>
-      <div className="card-blob"></div>
-      <div className="small-card-icon-wrapper">
-        <div className={`small-card-icon ${smIconClass}`}></div>
+      <div className="smcard-border">
+        <div className={`small-card-container ${smBackgroundClass}`}>
+          <div className="card-blob"></div>
+          <div className="small-card-icon-wrapper">
+            <div className={`small-card-icon ${smIconClass}`}></div>
+          </div>
+          <h3>{title}</h3>
+          <p>{text}</p>
         </div>
-      <h3>{title}</h3>
-      <p>{text}</p>
-    </div>
-    </div>
-    </div>
+      </div>
     </>
-      
-  
-  )
+  );
 }
 
-export default SmallCard
+export default SmallCard;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import Button from "../button/Button";
+import SmallCard from "../cards/smallCard/SmallCard";
 import Tools from "../tools/Tools";
 import "./header.css";
 
@@ -20,7 +21,11 @@ function Header() {
         </div>
         <div className="hero-border-shadow">
           <div className="hero-container">
-            <img className="hero-img" src="/images/planet-earth-background.jpg" alt="" />
+            <img
+              className="hero-img"
+              src="/images/planet-earth-background.jpg"
+              alt=""
+            />
             <div className="oval-gradient4"></div>
             <div className="oval-gradient5"></div>
             <p className="hero-subtext">
@@ -29,8 +34,30 @@ function Header() {
             </p>
             <Tools></Tools>
           </div>
+          <div className="small-cards-container">
+            <SmallCard
+              smBackgroundClass="smallbg1"
+              smIconClass="smallicon1"
+              title="Rådgivning och Workshops"
+              text="Från tanke till verklighet - strategisk planering i
+samarbete med kund"
+            />
+            <SmallCard
+              smBackgroundClass="smallbg2"
+              smIconClass="smallicon2"
+              title="Design och Utveckling"
+              text="Skapa användarvänliga lösningar med skalbar
+och robust kod"
+            />
+            <SmallCard
+              smBackgroundClass="smallbg3"
+              smIconClass="smallicon3"
+              title="Support och Underhåll"
+              text="Säkra långsiktig framgång med kontinuerlig
+optimering och support"
+            />
+          </div>
         </div>
-        
       </header>
     </>
   );
